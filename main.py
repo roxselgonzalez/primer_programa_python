918579d3f9298516aab8b46942cf8119a18407d7
roxselgonzalez <153641230+roxselgonzalez@users.noreply.github.com>
2023-12-12 11:19:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+# Este programa tiene como proposito poner en practica todo lo aprendido en clase con respecto a python como el
+#  manejo de tipos de datos, creacion de variables y comentarios Ejercicio 1: Operaciones basicas y tipos de datos
+#  Suma dos numeros enteros
+a = 10
+b = 30
+c = (a + b)
+print(c)
+# Resta dos numeros de punto flotante
+d = 10.6
+e = 2.3
+f = (d - e)
+print(f)
+# Multiplica dos numeros enteros
+g = 50
+h = 300
+i = (g * h)
+print(i)
+# Divide dos numeros punto flotante
+j = 50.4
+k = 10.2
+l = (j / k)
+print(l)
+# Ejercicio 2: Manipulacion de cadenas de texto
+# Declara una variable de tipo string que contenga tu nombre completo
+nc = "Roxsel Anahi Gonzalez Pesantez"
+# Utiliza la funcion len() para obtener la longitud de tu nombre completo
+long = len(nc)
+# Imprime tu nombre completo y su longitud
+print(nc)
+print(long)
+# Ejercicio 3: Conversion de tipos de datos
+# Declara un variable entera y otra punto flotante
+var1 = 100
+var2 = 200.5
+# Realiza la conversion de la variable entera a punto flotante y viceversa
+float(var1)
+int(var2)
+# Imprimir las variable originales
+print(var1)
+print(var2)
+# Imprime las variable convertidas
+print(float(var1))
+print(int(var2))
+# Ejercicio 4: Comentarios y documentacion
+# Importar el codigo en GitHub
